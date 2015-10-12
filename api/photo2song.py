ce8dc259c848b9fc4c46e8ddec1945ede551fdc2
icoxfog417 <icoxfog417@yahoo.co.jp>
2015-10-12 15:21:59 +0900

    implements parallel load from spotify

--- api/photo2song.py
+++ api/photo2song.py
@@ -1,4 +1,4 @@
-import re
+import asyncio
 from collections import Counter
 from api.bluemix_vision_recognition import VisionRecognizer
 from api.echonest import Echonest
@@ -16,21 +16,24 @@ def convert(image_urls):
 
     # analyze moode
     moods = Counter()
-    for url in image_urls:
-        result = vr.recognize(url)
-        vector = VisionRecognizer.to_matrix(result, TARGET_LABELS)[0]
-        mood = photo_to_mood.predict(vector)[0]
-        moods[mood] += 1
+    matrix = vr.recognize(image_urls).to_matrix(TARGET_LABELS)
+    for r in matrix:
+        mood = photo_to_mood.predict(r)[0]
+        moods[int(mood)] += 1
 
-    target_mood = moods.most_common(1)[0][0]
-    target_mood = Echonest.MOOD[int(target_mood)]
+    target_mood = moods.most_common(1)[0][0]  # get top and its score
+    target_mood = Echonest.MOOD[target_mood]
 
     # choose song from mood
     tracks = ec.search_songs(target_mood)
 
     # load spotify info
-    for t in tracks:
+    @asyncio.coroutine
+    def load_spotify(t):
         t.load_spotify(sp)
+
+    tasks = [load_spotify(t) for t in tracks]
+    done, _ = asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
 
     result = {
         "mood": target_mood,
--- tests/test_photo2song.py
+++ tests/test_photo2song.py
@@ -10,5 +10,5 @@ class TestPhoto2Song(unittest.TestCase):
             "https://oxfordportal.blob.core.windows.net/vision/Thumbnail/4.jpg"
         ]
         result = p2s.convert(image_urls)
-        self.assertTrue("results" in result)
+        self.assertTrue("mood" in result)
         print(result)
